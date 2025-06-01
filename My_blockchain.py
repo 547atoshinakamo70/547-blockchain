63671398274244a28665a5d9c74a353f46594d43
Rami bond <ramibaydoun@proton.me>
2025-06-01 03:20:36 +0100

    Update My_blockchain.py

--- My_blockchain.py
+++ My_blockchain.py
@@ -126,6 +126,19 @@ class Block:
     def to_dict(self):
         return {"index": self.index, "transactions": [t.to_dict() for t in self.transactions],
                 "timestamp": self.timestamp, "previous_hash": self.previous_hash, "nonce": self.nonce, "hash": self.hash}
+    def generate_key_pair():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private_key.public_key()
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ).decode('utf-8')
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ).decode('utf-8')
+    return private_pem, public_pem
 
 # Clase Blockchain con mejoras
 class Blockchain:
